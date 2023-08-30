6510ed8c1b4e7924043b846da316cf036ec42de7
Pete Berryman <peteb206@gmail.com>
2023-08-30 09:48:28 -0500

    small improvements in app.py

--- app.py
+++ app.py
@@ -11,19 +11,30 @@ print(' complete after', round((datetime.now() - now).seconds, 1), 'seconds')
 
 log_reg = BTSBatterClassifier(None, enhanced_at_bats, 'log_reg')
 
+grey_hex = '#D3D3D3'
+
 app_ui = ui.page_fluid(
     ui.tags.style(
+        f'''
+        body {{
+            background-color: {grey_hex};
+        }}
+        div.card-body {{
+            background-color: {grey_hex};
+        }}
+        tbody {{
+            background-color: white;
+        }}
+        div.shiny-data-grid-summary {{
+            color: green;
+        }}
+        a.nav-link {{
+            color: green !important;
+        }}
         '''
-        body {
-            background-color: #53565B;
-        }
-        '''
-        # div.card {
-        #     background-color: green;
-        # }
     ),
     # Reference this for adding JS (column widths, for example): https://shinylive.io/py/examples/#wordle
-    ui.h2('Beat the Streak', {'style': 'color: white;'}),
+    ui.h2('Beat the Streak', {'style': 'color: green;'}),
     ui.navset_tab_card(
         ui.nav(
             'Home',
@@ -36,25 +47,18 @@ app_ui = ui.page_fluid(
             ui.h3('Today\'s Picks', {'style': 'color: green;'}),
             ui.output_data_frame('todays_picks')
         ),
-        ui.nav('Player', '')
+        # ui.nav('Player', '')
     ),
     title = 'Beat the Streak Hub',
     lang = 'en'
 )
 
 def server(input, output, session):
-    # db_summary = list(__DB__.games.aggregate([
-    #     {
-    #         '$group': {
-    #             '_id': None,
-    #             'min': {'$min': '$game_date'},
-    #             'max': {'$max': '$game_date'},
-    #             'count': {'$sum': 1}
-    #         }
-    #     }, {
-    #         '$unset': '_id'
-    #     }
-    # ]))[0]
+    def timestamp_to_str(timestamp):
+        hour = str(timestamp.hour - (13 if timestamp.hour > 13 else 1))
+        minute = str(timestamp.minute) if timestamp.minute > 9 else f'0{timestamp.minute}'
+        am_pm = 'PM' if timestamp.hour > 12 else 'AM'
+        return f'{hour}:{minute} {am_pm}'
 
     @output
     @render.data_frame
@@ -62,11 +66,11 @@ def server(input, output, session):
         todays_predictions_df = log_reg.todays_predictions()
         df = todays_predictions_df.reset_index()
         df['game'] = df.apply(lambda row: f'{row["team"]} {"vs" if row["home"] else "@"} {row["opponent"]}', axis = 1)
-        df['time'] = df.apply(lambda row: f'{row["game_date"].hour - 13}:' + (str(row["game_date"].minute) if row["game_date"].minute > 9 else f'0{row["game_date"].minute}') + f' {"P" if row["game_date"].hour > 12 else "A"}M', axis = 1)
+        df['time'] = df.game_date.apply(lambda x: timestamp_to_str(x))
         df.lineup = df.lineup.apply(lambda x: 'OUT' if x == 10 else 'TBD' if x == 0 else x)
         df['H%'] = df['H%'].apply(lambda x: f'{round(x * 100, 1)}%')
-        df = df[['name', 'H%', 'game', 'time', 'opp_sp_name', 'lineup']]
-        df.columns = ['Batter', 'H%', 'Game', 'Time', 'Starting Pitcher', 'Lineup']
+        df = df[['name', 'H%', 'game', 'time', 'lineup', 'opp_sp_name']]
+        df.columns = ['Batter', 'H%', 'Game', 'Time', 'Lineup', 'Opposing Starter']
         return render.DataGrid(df, summary = 'Viewing Batters {start} to {end} of {total} | All times Central', row_selection_mode = 'none')
 
 app = App(app_ui, server)
